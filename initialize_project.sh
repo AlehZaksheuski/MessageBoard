d94dbe7a7e5d323bcf224e057ecf4674619f9630
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2017-09-30 22:43:58 +0300

    #master. Add basic initial script and test fixture, rename requirements.txt

---
+++ initialize_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install virtualenv
+sudo apt-get install python3
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py loaddata initial_data.json
+python manage.py migrate
+python manage.py runserver
