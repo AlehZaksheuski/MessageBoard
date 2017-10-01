c897e7ed6725d7f31c7b402cc5f1f6a9514d9f4a
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2017-10-01 14:10:16 +0300

    #master. Update staticfiles

--- settings_app/settings.py
+++ settings_app/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
