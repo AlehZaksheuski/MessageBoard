111eb6077af832e19d941359dd6e088a0a23383e
Aleh Zaksheuski <aleh.zaksheuski@leverx.com>
2017-10-01 18:20:31 +0300

    #master. Move back venv logic

--- initialize_project.sh
+++ initialize_project.sh
@@ -3,3 +3,11 @@ if ! [ "$1" = "--no-install" ]; then
     sudo apt-get install virtualenv
     sudo apt-get install python3
 fi
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata --exclude=contenttypes --exclude=auth.Permission initial_data.json
+python manage.py collectstatic --noinput
+python manage.py runserver
